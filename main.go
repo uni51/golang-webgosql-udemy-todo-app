0eab8b695a4db8bd231bb177c2bf20e1e1bd559a
uni51 <unieye51@gmail.com>
2023-10-04 14:00:10 +0900

    2-3 User.go GetUser

--- main.go
+++ main.go
@@ -14,4 +14,17 @@ func main() {
 	// log.Println("test")
 
 	fmt.Println(models.Db)
+
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
+
+		u.CreateUser()
+	*/
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
