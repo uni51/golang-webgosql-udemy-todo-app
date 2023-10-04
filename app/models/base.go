7688b3c2301d147ca24f2153d709e7c693b32d2e
uni51 <unieye51@gmail.com>
2023-10-04 14:13:21 +0900

    3-1 Todo.go CreateTodo

--- app/models/base.go
+++ app/models/base.go
@@ -18,6 +18,7 @@ var err error
 
 const (
 	tableNameUser = "users"
+	tableNameTodo = "todos"
 )
 
 // main関数の前に実行される
@@ -36,6 +37,14 @@ func init() {
 		created_at DATETIME)`, tableNameUser)
 
 	Db.Exec(cmdU)
+
+	cmdT := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		content TEXT,
+		user_id INTEGER,
+		created_at DATETIME)`, tableNameTodo)
+
+	Db.Exec(cmdT)
 }
 
 func createUUID() (uuidobj uuid.UUID) {
