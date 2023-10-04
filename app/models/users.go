dd10c520879c42509f6300656d1bcca56b2b71b7
uni51 <unieye51@gmail.com>
2023-10-04 14:08:28 +0900

    2-5. User.go DeleteUser

--- app/models/users.go
+++ app/models/users.go
@@ -49,3 +49,21 @@ func GetUser(id int) (user User, err error) {
 	)
 	return user, err
 }
+
+func (u *User) UpdateUser() (err error) {
+	cmd := `update users set name = ?, email = ? where id = ?`
+	_, err = Db.Exec(cmd, u.Name, u.Email, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `delete from users where id = ?`
+	_, err = Db.Exec(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
--- main.go
+++ main.go
@@ -27,4 +27,16 @@ func main() {
 
 	u, _ := models.GetUser(1)
 	fmt.Println(u)
+
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
+
+	u.DeleteUser()
+
+	u, _ = models.GetUser(1)
+	fmt.Println(u)
 }
